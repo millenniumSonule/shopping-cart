8c7d164a995f7f3081759b871cf4a50d70d60016
millenniumSonule <anshumansonuley@gmail.com>
2024-05-14 21:29:59 +0530

    .

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import logo from './Assets/fake-logo.png'
+import cart from './Assets/cart.png'
+import './navbar.css'
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 const Navbar = () => {
   return (
-    <div>navbar</div>
+    <div className="nav-bar">
+      <div className="logo-navbar">
+        <img src={logo} alt="Logo" />
+        <div className='nav-texts'><Link to="/">Products</Link></div>
+      </div>
+
+      <div className='nav-texts '>
+        <Link className=' adjust-cart' to="/cart"><img className='cart-logo' src={cart} alt="Logo" />
+        </Link>
+        <div className='total-cart'>
+          {12}
+        </div>
+      </div>
+
+    </div>
   )
 }
 
