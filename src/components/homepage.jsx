260d623c1bd55ea1ded018e3b9aa4c5bf57d6043
millenniumSonule <anshumansonuley@gmail.com>
2024-05-14 21:29:49 +0530

    .

--- src/components/homepage.jsx
+++ src/components/homepage.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import './homepage.css'
+import Navbar from './navbar'
 
 const Homepage = () => {
   return (
-    <div>homepage</div>
+    <div className="homepage-container">
+      <div className="navbar-top">
+        
+        <Navbar/>
+      </div>
+     
+      <div className="plp-page">
+
+        
+      </div>
+    </div>
   )
 }
 
